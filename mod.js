399318a8ac9cbf2a9f94e7d8cf043a5b3a133cea
EL2020 <el2020202020@gmail.com>
2021-04-21 21:56:37 -0400

    Added many items! (Booster now uses custom item!)

--- mod.js
+++ mod.js
@@ -1,6 +1,6 @@
 sc.EnemyBooster.inject({
     updateEnemyBoostState(b){
-        if (b.boosterState != sc.ENEMY_BOOSTER_STATE.NONE)
+        if (b.boosterState != sc.ENEMY_BOOSTER_STATE.NONE){
             if (this.boosted && b.boosterState == sc.ENEMY_BOOSTER_STATE.BOOSTABLE) {
                 b.boosterState = sc.ENEMY_BOOSTER_STATE.BOOSTED;
                 var a = b.enemyType.boostedLevel || sc.MIN_BOOSTER_LEVEL;
@@ -9,8 +9,8 @@ sc.EnemyBooster.inject({
             } else if (!this.boosted && b.boosterState == sc.ENEMY_BOOSTER_STATE.BOOSTED) {
             b.boosterState = sc.ENEMY_BOOSTER_STATE.BOOSTABLE;
             b.level.setting ? b.setLevelOverride(1 * ig.Event.getExpressionValue(b.level.setting)) : b.setLevelOverride(null)
-        }
-        if (sc.model.player.getToggleItemState(618)) {
+        }}
+        if (sc.model.player.getToggleItemState("dlctweaks-ascended-booster") && b.boosterState == sc.ENEMY_BOOSTER_STATE.BOOSTABLE) {
             b.setLevelOverride(!sc.newgame.get("scale-enemies") ? sc.model.player.level : sc.model.player.getParamAvgLevel(10))
             b.boosterState = sc.ENEMY_BOOSTER_STATE.BOOSTED;
         }
