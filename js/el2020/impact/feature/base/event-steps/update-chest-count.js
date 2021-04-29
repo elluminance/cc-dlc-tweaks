1ebd2b949d8a0b41f218e2bbbacf1ba46b90fd52
EL2020 <el2020202020@gmail.com>
2021-04-29 11:25:18 -0400

    added common event for chest updating

--- js/el2020/impact/feature/base/event-steps/update-chest-count.js
+++ js/el2020/impact/feature/base/event-steps/update-chest-count.js
@@ -4,9 +4,10 @@ ig.module("el2020.impact.feature.base.event-steps.update-chest-count").requires(
             this.map = a.map;
             this.area = a.area;
             this.chestID = a.chestID;
+            this.variable = a.variable
         },
         start: function(){
-            let b = "maps." + this.map.toPath("", "").toCamel() + ".chest_" + this.chestID;
+            let b = "maps." + this.map.toPath("", "").toCamel() + "." + (this.variable ?? ("chest_" + this.chestID));
             if(ig.vars.get(b)){
                 sc.stats.addMap("chests", this.area, 1)
                 sc.stats.addMap("chests", "total", 1)
