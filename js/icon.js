5d67b92714ca1395037f993ae785723f8aa02860
EL2020 <el2020202020@gmail.com>
2021-09-28 11:58:50 -0400

    merged content of extra skins

--- js/icon.js
+++ js/icon.js
@@ -1,8 +1,9 @@
-const ascBoosterIcons = new ig.Font("media/font/el-ascended-booster.png", 16, ig.MultiFont.ICON_START);
+const icons = new ig.Font("media/font/els-mod-icons.png", 16, ig.MultiFont.ICON_START);
 
 const fontIndex = sc.fontsystem.font.iconSets.length;
-sc.fontsystem.font.pushIconSet(ascBoosterIcons)
+sc.fontsystem.font.pushIconSet(icons)
 
 sc.fontsystem.font.setMapping({
-    "item-toggle-scale": [fontIndex, 0]
+    "item-toggle-scale": [fontIndex, 0],
+    "el-rainbow-text": [fontIndex, 1]
 })
