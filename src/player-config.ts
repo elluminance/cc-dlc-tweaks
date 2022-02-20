a2de82d35616de04c562eac5aa7bdb2367d53fdf
EL2020 <el2020202020@gmail.com>
2022-02-20 18:35:45 -0500

    added triblader attacks

--- src/player-config.ts
+++ src/player-config.ts
@@ -1,3 +1,8 @@
+/*
+ * this is just so Arcane Lab doesn't treat the vanilla neutral arts as 
+ * being modded arts - simply just "copying" the vanilla data into an identical
+ * action that has a different internal name. it's a silly solution - but hey, it works!
+ */
 sc.PlayerConfig.inject({
     onload(data) {
         this.parent(data);
@@ -5,7 +10,7 @@ sc.PlayerConfig.inject({
         for(const type of ["ATTACK", "THROW", "DASH", "GUARD"]) {
             for(const identifier of ["1_A", "1_B", "2_A", "2_B"]) {
                 this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}_COPY`] =
-                    this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}`] ;
+                    this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}`];
             }
         }
     }
