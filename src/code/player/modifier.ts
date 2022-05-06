d123c90b3162918553007b0f106d4b38853f657c
EL2020 <el2020202020@gmail.com>
2022-05-06 13:03:52 -0400

    finished lifesteal reworks

--- src/code/player/modifier.ts
+++ src/code/player/modifier.ts
@@ -16,8 +16,7 @@ export default function () {
             offX: 12 * 1,
             offY: 0,
             icon: -1,
-            order: 0,
-            noPercent: true
+            order: 0
         },
 
         EL_GEODE_FINDER: {
@@ -97,36 +96,75 @@ export default function () {
     }
 
     //#region vampirism
+    const lifestealCooldown = 1/5;
+    // this constant ensures that (0.9^(a*t) - 1) = 1 at max cooldown
+    const vampirismConstant = Math.log(2) / (Math.log(0.9) * lifestealCooldown) 
+
     sc.CombatParams.inject({
+        el_lifestealHealed: 0,
+        el_lifestealTimer: 0,
+
+        update(inCombat) {
+            this.parent(inCombat);
+            this.el_lifestealTimer -= ig.system.tick;
+        },
+
         getDamage(attackInfo, damageFactorMod, combatant, shieldResult, j) {
             const damageResult = this.parent(attackInfo, damageFactorMod, combatant, shieldResult, j);
 
-            let combatantParams = combatant.getCombatantRoot().params;
+            let combatantParams = combatant.getCombatantRoot().params,
+                lifesteal = combatantParams.getModifier("EL_LIFESTEAL");
             // the this.combatant !== combatant is simply to make sure that any self inflicted damage (i.e. jolt)
             // does not trigger life steal. wouldn't make sense to steal from yourself, y'know?
-            if (combatantParams.getModifier("EL_LIFESTEAL") && (this.combatant !== combatant.getCombatantRoot())) {
-                let relativeDamage = damageResult.damage * 0.075;
+            if (lifesteal > 0 && (this.combatant !== combatant.getCombatantRoot())) {
+                let relativeDamage = damageResult.damage * 0.05;
 
-                if(sc.newgame.get("sergey-hax") && !ig.vars.get("g.newgame.ignoreSergeyHax")) relativeDamage /= 4096
+                if (sc.newgame.get("sergey-hax") 
+                 && !ig.vars.get("g.newgame.ignoreSergeyHax") 
+                 && combatant.getCombatantRoot().isPlayer
+                ) relativeDamage /= 4096
 
                 // avoid division by 0
                 relativeDamage = relativeDamage !== 0 ? relativeDamage / Math.log1p(relativeDamage) : 0; 
-                relativeDamage *= 1 + combatantParams.getModifier("HP_REGEN").limit(-1, 1)
+                relativeDamage *= 1 + combatantParams.getModifier("HP_REGEN").limit(-1, 2) / 2;
+                relativeDamage *= 1 + lifesteal;
                 relativeDamage = Math.min(combatantParams.getStat("hp") / 10, relativeDamage);
-                let healAmount = this.getHealAmount({value: relativeDamage, absolute: true});
-                if(sc.options.get("damage-numbers")) {
-                    ig.ENTITY.HitNumber.spawnHealingNumber(combatant.getCombatantRoot().getAlignedPos(ig.ENTITY_ALIGN.CENTER, Vec3.create()), combatant.getCombatantRoot(), healAmount);
+                let adjustedHealth = Math.abs(combatantParams.el_lifestealHealed * (0.9 ** (vampirismConstant * combatantParams.el_lifestealTimer) - 1));
+                if ((combatantParams.el_lifestealTimer > 0) && (relativeDamage > adjustedHealth)) {
+                    combatantParams.el_lifestealHealed = relativeDamage;
+                    relativeDamage -= adjustedHealth;
+                } else if (combatantParams.el_lifestealTimer > 0) {
+                    relativeDamage = 0;
+                } else {
+                    combatantParams.el_lifestealHealed = relativeDamage;
                 }
-                this.increaseHp(healAmount)
-
-                //@ts-ignore
-                combatant.getCombatantRoot().effects.death.spawnOnTarget("el_lifesteal_steal", combatant,
-                    {
-                        target2: this.combatant,
-                        target2Align: ig.ENTITY_ALIGN['CENTER'],
-                        align: ig.ENTITY_ALIGN['CENTER']
+                relativeDamage = Math.floor(relativeDamage);
+                if(relativeDamage > 0) {
+                    let healAmount = this.getHealAmount({value: relativeDamage, absolute: true});
+                    
+                    if(sc.options.get("damage-numbers")) {
+                        ig.ENTITY.HitNumber.spawnHealingNumber(
+                            combatant.getCombatantRoot().getAlignedPos(ig.ENTITY_ALIGN.CENTER, Vec3.create()), 
+                            combatant.getCombatantRoot(), 
+                            healAmount
+                        );
                     }
-                );
+                    
+                    this.increaseHp(healAmount)
+    
+                    //@ts-ignore
+                    combatant.getCombatantRoot().effects.death.spawnOnTarget("el_lifesteal_steal", combatant,
+                        {
+                            target2: this.combatant,
+                            target2Align: ig.ENTITY_ALIGN['CENTER'],
+                            align: ig.ENTITY_ALIGN['CENTER']
+                        }
+                    );
+                    //@ts-ignore
+                    this.combatant.effects.death.spawnOnTarget("el_lifesteal_aura", this.combatant);
+                    
+                    combatantParams.el_lifestealTimer = lifestealCooldown;
+                }
             }
             return damageResult;
         }
@@ -135,7 +173,7 @@ export default function () {
     ig.ENTITY.Player.inject({
         updateModelStats(a) {
             this.parent(a)
-            if (this.params.getModifier("EL_LIFESTEAL")) this.regenFactor = 0;
+            if (this.params.getModifier("EL_LIFESTEAL") > 0) this.regenFactor = 0;
             if (this.params.getModifier("EL_TRANCE")) this.regenFactor = 0;
         },
 
