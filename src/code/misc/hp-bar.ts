c7564bdd394e6a24d2ca2be2ed7072e315ebb3f1
EL2020 <el2020202020@gmail.com>
2022-05-16 23:50:28 -0400

    added hp bar to status menu

--- src/code/misc/hp-bar.ts
+++ src/code/misc/hp-bar.ts
@@ -1,50 +1,52 @@
 import { getColorFromPercent } from "../../helper-funcs.js";
 
+export function DrawHpBar(renderer: ig.GuiRenderer, currentValue: number, targetValue: number, maxValue: number, width: number, height: number, x = 0, y = 0): void { 
+    let hpRatio = currentValue / maxValue,
+        targetRatio = targetValue / maxValue,
+        maxRatio = Math.max(hpRatio, targetRatio);
+    if (hpRatio > 1) {
+        for (let i = Math.max(Math.floor(maxRatio) - 2, 1); i < maxRatio; i++) {
+            let isFullCurrent = i <= (hpRatio - 1),
+                isEmptyCurrent = i > hpRatio,
+                isFullTarget = i <= (targetRatio - 1),
+                isEmptyTarget = i > targetRatio,
+                color = getColorFromPercent(270, 210, i - 1),
+                widthWhite: number,
+                widthColor: number,
+                useWhite = true;
+
+            if (targetValue < currentValue) {
+                widthWhite = isFullCurrent ? 1 : isEmptyCurrent ? 0 : (hpRatio % 1);
+                widthColor = isFullTarget ? 1 : isEmptyTarget ? 0 : (targetRatio % 1);
+            } else {
+                widthWhite = isFullTarget ? 1 : isEmptyTarget ? 0 : (targetRatio % 1);
+                widthColor = isFullCurrent ? 1 : isEmptyCurrent ? 0 : (hpRatio % 1);
+                useWhite = false;
+            }
+            for (let j = 0; j < height; j++) {
+                if (widthWhite && targetValue != currentValue) {
+                    useWhite || renderer.addColor(color, x + j, y + j, widthWhite * width, 1)
+                    renderer.addColor("#fff", x + j, y + j, widthWhite * width, 1).setAlpha(useWhite ? 0.75 : 0.6)
+                }
+                renderer.addColor(color, x + j, y + j, widthColor * width, 1)
+            }
+        }
+    }
+}
+
 export default function () {
     sc.HpHudBarGui.inject({
-        hasOverheal: false,
-
-        update() {
-            this.parent()
-
-            if(this.hasOverheal && (Math.max(this.targetHp, this.currentHp) < this.maxHp)) {
-                this.hasOverheal = false;
-            } else if (this.targetHp > this.maxHp) {
-                this.hasOverheal = true;
-            }
-        },
         updateDrawables(renderer) {
             this.parent(renderer);
+            DrawHpBar(renderer, this.currentHp, this.targetHp, this.maxHp, this.width, this.height)
+        }
+    })
 
-            let hpRatio = this.currentHp / this.maxHp,
-                targetRatio = this.targetHp / this.maxHp;
-            if (this.hasOverheal && (hpRatio > 1 || targetRatio > 1)) {
-                for (let i = Math.max(Math.floor(hpRatio) - 2, 1); i < hpRatio; i++) {
-                    let isFullCurrent = (hpRatio - i >= 1) || (hpRatio % 1 == 0),
-                        isEmptyCurrent = (hpRatio - i < 0),
-                        isFullTarget = (targetRatio - i >= 1) || (targetRatio % 1 == 0),
-                        isEmptyTarget = (targetRatio - i) < 0,
-                        color = getColorFromPercent(270, 210, i - 1),
-                        widthWhite: number,
-                        widthColor: number,
-                        useWhite = true;
-
-                    if (this.targetHp < this.currentHp) {
-                        widthWhite = isFullCurrent ? 1 : isEmptyCurrent ? 0 : ((this.currentHp / this.maxHp) % 1);
-                        widthColor = isFullTarget ? 1 : isEmptyTarget ? 0 : ((this.targetHp / this.maxHp) % 1);
-                    } else {
-                        widthWhite = isFullTarget ? 1 : isEmptyTarget ? 0 : ((this.targetHp / this.maxHp) % 1);
-                        widthColor = isFullCurrent ? 1 : isEmptyCurrent ? 0 : ((this.currentHp / this.maxHp) % 1);
-                        useWhite = false;
-                    }
-                    for (let j = 0; j < this.height; j++) {
-                        if (widthWhite && this.targetHp != this.currentHp) {
-                            useWhite || renderer.addColor(color, j, j, widthWhite * this.width, 1)
-                            renderer.addColor("#fff", j, j, widthWhite * this.width, 1).setAlpha(useWhite ? 0.9 : 0.75)
-                        }
-                        renderer.addColor(color, j, j, widthColor * this.width, 1)
-                    }
-                }
+    sc.ItemStatusDefaultBar.inject({
+        updateDrawables(renderer) {
+            this.parent(renderer)
+            if(this.type === sc.MENU_BAR_TYPE.HP) {
+                DrawHpBar(renderer, this.currentValue, this.targetValue, this.maxValue, 119, 4, 2, 9)
             }
         }
     })
