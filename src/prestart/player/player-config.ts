b391b95a9613c6ce6027e8a60e12c9cfbe358c02
EL2020 <el2020202020@gmail.com>
2022-12-26 13:33:20 -0500

    fixed c'tron

--- src/prestart/player/player-config.ts
+++ src/prestart/player/player-config.ts
@@ -6,11 +6,12 @@
 sc.PlayerConfig.inject({
     onload(data) {
         this.parent(data);
-        if (this.name !== "Lea") return;
-        for (const type of ["ATTACK", "THROW", "DASH", "GUARD"]) {
-            for (const identifier of ["1_A", "1_B", "2_A", "2_B"]) {
-                this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}_COPY`] =
-                    this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}`];
+        if (this.name === "Lea") {
+            for (const type of ["ATTACK", "THROW", "DASH", "GUARD"]) {
+                for (const identifier of ["1_A", "1_B", "2_A", "2_B"]) {
+                    this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}_COPY`] =
+                        this.elementConfigs[sc.ELEMENT.NEUTRAL].actions[`${type}_SPECIAL${identifier}`];
+                }
             }
         }
     }
